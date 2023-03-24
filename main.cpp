d2f275afd944508c1614124d5740d0a7400b8993
Alex Moreno <alexmijowastaken@gmail.com>
2023-03-24 16:16:22 -0500

    Solved 2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 #include "1.cpp"
+#include "2.cpp"
 
 int main() {
     std::cout << "Problem 1:\n" << problem1() << std::endl;
+    std::cout << "Problem 2:\n" << problem2() << std::endl;
 }
